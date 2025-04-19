a3d811e1d555ef248a2133b75d6b9bf8fcf13d47
palihabera <107116436+palihabera@users.noreply.github.com>
2025-04-19 18:04:38 +0200

    test

---
+++ hello.js
@@ -0,0 +1,11 @@
+fetch("https://dev.hey.bild.de/api/search?query=a", {
+  method: "GET",
+  credentials: "include"
+})
+.then(async (response) => {
+  const text = await response.text();
+  document.getElementById("output").textContent = `✅ SUCCESS:\n\n${text}`;
+})
+.catch((error) => {
+  document.getElementById("output").textContent = `❌ ERROR:\n\n${error}`;
+});
