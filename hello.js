d03c2703d816c63a008de43ed93abec3d9afda9f
palihabera <107116436+palihabera@users.noreply.github.com>
2025-04-19 21:23:30 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,12 +1,12 @@
-fetch("https://whoami-api.prod.ps,bild.de/api/refresh", {
+fetch("https://whoami-api.prod.ps.bild.de/api/refresh", {
   method: "POST",
   credentials: "include",
   headers: {
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
-    "grant_type":"refresh_token"
-  }
+    "grant_type": "refresh_token"
+  })
 })
 .then(async (response) => {
   const text = await response.text();
