3e0173ce8f0a8e26487106b3c2bd00fc67524bee
palihabera <107116436+palihabera@users.noreply.github.com>
2025-05-31 20:13:46 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,10 +2,6 @@ document.getElementById("fetchButton").addEventListener("click", () => {
   fetch("https://whoami-api.prod.ps.bild.de/api/refresh?returnTo=https://dev.hey.bild.de", {
     method: "GET",
     credentials: "include",
-    headers: {
-      "Cache-Control": "no-cache",
-      "Pragma": "no-cache"
-    },
   })
   .then(async (response) => {
     const text = await response.text();
